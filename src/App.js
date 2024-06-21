8f1d6efe210baa8d2d9e13aaaa712ac19e1dd07c
Luniva Shrestha <shresthaluniva73@gmail.com>
2024-06-21 14:40:18 +0545

    colorGenerator page

--- src/App.js
+++ src/App.js
@@ -1,33 +1,43 @@
-import "./App.css";
-import Card from "./card";
-import Calculator from "./calculator";
-import Todo from "./todo";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import "./App.css";
+// import LogOnMount from "./LogonMount";
+// import Timer from "./Timer";
+// import DynamicTitle from "./DynamicTitle";
+import ColorGenerate from "./ColorGenerate";
+// import Card from "./card";
+// import Calculator from "./calculator";
+// import Todo from "./todo";
+// import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/calculator">Calculator</Link>
-            </li>
-            <li>
-              <Link to="/card">Card</Link>
-            </li>
-            <li>
-              <Link to="/todo">Todo</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/calculator" element={<Calculator />} />
-          <Route path="/card" element={<Card />} />
-          <Route path="/todo" element={<Todo />} />
-        </Routes>
-      </div>
-    </Router>
+    <div>
+      <ColorGenerate />
+      {/* <LogOnMount/>
+      <Timer />
+      <DynamicTitle /> */}
+    </div>
+    //   <Router>
+    //     <div>
+    //       <nav>
+    //         <ul>
+    //           <li>
+    //             <Link to="/calculator">Calculator</Link>
+    //           </li>
+    //           <li>
+    //             <Link to="/card">Card</Link>
+    //           </li>
+    //           <li>
+    //             <Link to="/todo">Todo</Link>
+    //           </li>
+    //         </ul>
+    //       </nav>
+    //       <Routes>
+    //         <Route path="/calculator" element={<Calculator />} />
+    //         <Route path="/card" element={<Card />} />
+    //         <Route path="/todo" element={<Todo />} />
+    //       </Routes>
+    //     </div>
+    //   </Router>
   );
 }
 export default App;
---
+++ src/LogonMount.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+
+function LogOnMount() {
+  useEffect(() => {
+    console.log("Component has mounted!");
+  }, []); // Empty array means this effect runs only on mount and unmount
+
+  return <h1>Welcome to the page!</h1>;
+}
+
+export default LogOnMount;
