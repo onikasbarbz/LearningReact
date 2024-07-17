223ceb583534b283c387a1c825e51b6fe9465d10
Luniva Shrestha <shresthaluniva73@gmail.com>
2024-07-17 11:35:45 +0545

    material ui, National Park Rating page

--- src/Mui.jsx
+++ src/Mui.jsx
@@ -6,62 +6,104 @@ import Typography from "@mui/material/Typography";
 import Chip from "@mui/material/Chip";
 import Rating from "@mui/material/Rating";
 import Switch from "@mui/material/Switch";
+import yosemite from "./asset/yosemite.jpg";
+import glacier from "./asset/glacier.jpg";
+import grand from "./asset/grand.jpg";
+import yellow from "./asset/yellow.jpg";
+import { styled } from "@mui/material/styles";
 
-
+const CustomRating = styled(Rating)({
+  "& .MuiRating-iconFilled": {
+    color: "#ffb400",
+  },
+  "& .MuiRating-iconHover": {
+    color: "#ffb400",
+  },
+  "& .MuiRating-iconEmpty": {
+    color: "#b0b0b0", // Grey color for the border of empty stars
+  },
+});
 const Mui = () => {
-  const images = [
+  const places = [
     {
-      img: "./asset/yosemite.jpg",
+      img: yosemite,
       name: "yosemite",
       typo: "Yosemite National Park, California, USA",
     },
     {
-      img: "./asset/yosemite.jpg",
-      name: "yosemite",
-      typo: "Glacier National Park,Montana,USA",
+      img: glacier,
+      name: "glacier",
+      typo: "Glacier National Park, Montana, USA",
     },
     {
-      img: "./asset/yosemite.jpg",
-      name: "yosemite",
-      typo: "Grand canyon National Park,Arizona, USA",
+      img: grand,
+      name: "grand",
+      typo: "Grand canyon National Park, Arizona, USA",
     },
     {
-      img: "./asset/yosemite.jpg",
-      name: "yosemite",
-      typo: "Yellowstone National Park,Wyoming,USA",
+      img: yellow,
+      name: "yellow",
+      typo: "Yellowstone National Park, Wyoming, USA",
     },
   ];
-  const initialState = places.map(()=>false);
+  const initialState = places.map(() => false);
   const [active, setActive] = useState(initialState);
 
-  const handleSwitchChange = (event) => {
-    setActive(event.target.checked);
+  const handleSwitchChange = (index) => (event) => {
+    const newActive = [...active];
+    newActive[index] = event.target.checked;
+    setActive(newActive);
   };
 
   return (
-    <div>
-      {places.map((items,index)=>(
-      <Card>
-        <CardMedia
-          component="img"
-          alt="Yosemite National Park"
-          image="/asset/yosemite.jpeg"
-        />
-        <Stack direction="row" alignItems="center" spacing={3} p={2}>
-          <Stack direction="column" spacing={0.5}>
-            <Typography>{items.typo}</Typography>
-            <Stack direction="row" spacing={1}>
-              <Chip
-                size="small"
-                label={active[index] ? "Active" : "Inactive"}
-                color={active [index]? "success" : "default"}
-              />
-              <Rating defaultValue={1} size="small" />
+    <div
+      style={{
+        backgroundColor: "#202020",
+        minHeight: "200vh",
+        padding: "40px",
+        margin: "-30px",
+      }}
+    >
+      {places.map((items, index) => (
+        <Card
+          style={{
+            backgroundColor: active[index] ? "#454545" : "#303030",
+            marginBottom: "20px",
+            borderRadius: "10px",
+          }}
+        >
+          <CardMedia
+            component="img"
+            alt={items.name}
+            image={items.img}
+            style={{ height: "300px", width: "1355px", borderRadius: "5px" }}
+          />
+          <Stack direction="row" alignItems="center" spacing={3} p={2}>
+            <Stack direction="column" spacing={0.5}>
+              <Typography style={{ color: "white", fontSize: "1.5rem" }}>
+                {items.typo}
+              </Typography>
+              <Stack direction="row" spacing={1} alignItems="center">
+                <Chip
+                  size="medium"
+                  label={active[index] ? "Active" : "Inactive"}
+                  color={active[index] ? "success" : "default"}
+                  style={{
+                    backgroundColor: active[index] ? "#4CAF50" : "#686868",
+                    color: active[index] ? "black" : "white",
+                  }}
+                />
+                <CustomRating defaultValue={1} size="medium" />
+              </Stack>
             </Stack>
+            <div style={{ marginLeft: "auto" }}>
+              <Switch
+                checked={active[index]}
+                onChange={handleSwitchChange(index)}
+              />
+            </div>
           </Stack>
-          <Switch checked={active[index]} onChange={handleSwitchChange(index)} />
-        </Stack>
-      </Card>
+        </Card>
       ))}
     </div>
   );
